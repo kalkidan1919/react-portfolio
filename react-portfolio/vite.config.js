3a637f9030854e33ab61afefecd1a15014e8ebd9
kalkidan1919 <kalkalpro19@gmail.com>
2025-07-09 19:25:08 +0700

    i initialized gh-pages

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/react-portfolio/", // 👈 match the repo name exactly
   plugins: [react()],
-})
+});
